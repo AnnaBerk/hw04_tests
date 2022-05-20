b56767f0f9d6ffa25fb22a6c33540d0a99ae48b1
Бернова Анна <bast33@yandex.ru>
2022-05-20 13:09:37 +0300

    добавляет тест кэша

--- yatube/posts/tests/test_cache.py
+++ yatube/posts/tests/test_cache.py
@@ -1,47 +1,49 @@
-from django.test import TestCase, Client
+import shutil
+import tempfile
+
+from django.test import TestCase, Client, override_settings
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from django.conf import settings
 from django.core.cache import cache
 
 from ..models import Post
 
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
 User = get_user_model()
 
 
-class GroupViewTests(TestCase):
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
+class GroupCacheTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.user = User.objects.create_user(username='auth')
         cls.post = Post.objects.create(
             author=cls.user,
-            text='Тестовый пост больше 15 символов',
+            text='Тестовый пост',
         )
-        cls.post2 = Post.objects.create(
-            author=cls.user,
-            text='Тестовый пост больше 15 символов',
-        )
-
-    # @classmethod
-    # def tearDownClass(cls):
 
     def setUp(self):
         self.guest_client = Client()
-        self.authorized_client = Client()
-        self.authorized_client.force_login(self.user)
-   
-   
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
+
     def test_index_cache(self):
         """
         Проверяем что механизм кеша главной страницы работает
         """
         url = reverse('posts:index')
         response = self.guest_client.get(url)
-        count = len(response.context['page_obj'])
-        self.assertEqual(len(response.context['page_obj']), count)
-        # post = Post.objects.filter(id=1)
-        # post.delete()
-        # response = self.guest_client.get(url)
-        # count = len(response.context['page_obj'])
-        # self.assertEqual(len(response.context['page_obj']), count)
+        self.assertEqual(len(response.context['page_obj']), 1)
+        post = Post.objects.get(id=1)
+        post.delete()
+        response = self.guest_client.get(url)
+        self. assertIn(post.text, response.content.decode())
+        cache.clear()
+        response = self.guest_client.get(url)
+        self.assertNotIn(post.text, response.content.decode())
